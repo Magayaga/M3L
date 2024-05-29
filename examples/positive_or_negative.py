58a8571844acfd84b26bf5b158971d165c5a097d
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2024-05-29 13:58:53 +0800

    Create `positive_or_negative.py`

---
+++ examples/positive_or_negative.py
@@ -0,0 +1,9 @@
+from M3L import M3L
+
+# positive numbers
+print(M3L.positive(4))
+print(M3L.add(M3L.positive(4), M3L.positive(M3L.PI)))
+
+# negative numbers
+print(M3L.negative(9))
+print(M3L.add(M3L.negative(9), M3L.negative(M3L.PI)))
